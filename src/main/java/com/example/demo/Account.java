975608714f0d5514ffb50838272be94bf4cab860
yeonmong sung <yeonmong112@naver.com>
2019-08-22 17:12:20 +0900

    ggg

--- src/main/java/com/example/demo/Account.java
+++ src/main/java/com/example/demo/Account.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
 import javax.persistence.*;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 public class Account {
+
+
 
     @Id
     @GeneratedValue
